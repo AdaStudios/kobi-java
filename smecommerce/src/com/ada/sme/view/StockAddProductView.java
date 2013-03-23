f17a7ea13ec69b151a3956c6dbf8a0d6fc1f9880
EnesKıdık <nskdk2@gmail.com>
2013-03-23 14:42:05 +0200

    added StockAddProductView

---
+++ smecommerce/src/com/ada/sme/view/StockAddProductView.java
@@ -0,0 +1,34 @@
+package com.ada.sme.view;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tvoastro
+ * Date: 3/23/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StockAddProductView {
+    private JPanel stockAddProductViewContainerPanel;
+    private JLabel addProductLabel;
+    private JLabel productNameLabel;
+    private JLabel productModelLabel;
+    private JLabel productPriceLabel;
+    private JTextField productNameTextField;
+    private JLabel productDescription;
+    private JLabel productWeigth;
+    private JLabel productDimensions;
+    private JLabel productCategory;
+    private JLabel productQuantity;
+    private JComboBox productCategoryComboBox;
+    private JTextField productQuantityTextField;
+    private JTextField productModelTextField;
+    private JTextField productPrizeTextField;
+    private JTextPane productDescriptionTextPane1;
+    private JTextField productWeightTextField;
+    private JTextField productDimensionsTextField;
+
+
+}
+
