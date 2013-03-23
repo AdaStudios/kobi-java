3ac0a7d1796ad81cb977123a38d59e52a88c189d
EnesKidik <nskdk2@gmail.com>
2013-03-24 00:38:06 +0200

    added LEftMenuView , LeftStaffAltMenuView Local Store Staff Views

---
+++ smecommerce/src/com/ada/sme/view/LeftMenuView.java
@@ -0,0 +1,14 @@
+package com.ada.sme.view;
+
+import javax.swing.*;
+
+/**
+ * User: Enes Kıdık
+ * Date: 3/24/13 --  Time: 12:29 AM
+ */
+public class LeftMenuView
+{
+    private JPanel leftMenuContainerpanel;
+    private JButton salesButton;
+    private JButton staffButton;
+}
---
+++ smecommerce/src/com/ada/sme/view/LeftStaffAltMenuView.java
@@ -0,0 +1,15 @@
+package com.ada.sme.view;
+
+import javax.swing.*;
+
+/**
+ * User: Enes Kıdık
+ * Date: 3/24/13 --  Time: 12:32 AM
+ */
+public class LeftStaffAltMenuView
+{
+    private JPanel leftStaffAltMenuContainerPanel;
+    private JButton addStaffButton;
+    private JButton updateStaffButton;
+    private JButton deleteStaffButton;
+}
