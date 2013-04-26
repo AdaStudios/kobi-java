0350af26c3bdf54e4ebe5b186b5ee91ab9167a93
cagdas <cagdas@cagdas-R540>
2013-04-26 21:37:09 +0300

    Linux Initial

--- src/com/ada/sme/controller/OnlineDBController.java
+++ src/com/ada/sme/controller/OnlineDBController.java
@@ -15,34 +15,34 @@ import java.util.logging.Logger;
  * @author cagdas
  */
 public class OnlineDBController {
-        static Connection conn2 = null;
+
+    static Connection conn2 = null;
 
     public OnlineDBController() throws SQLException {
-            try {
-                /*try {
-                    Class.forName("com.mysql.jdbc.Driver");
-                    System.out.println("Driver loaded");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
 
-                    String url2 = "jdbc:mysql://139.179.139.112:3306/open";
-                    conn2 = DriverManager.getConnection(url2, "root", "ozyaz.14");
+            String url2 = "jdbc:mysql://139.179.139.112/open";
+            conn2 = DriverManager.getConnection(url2, "opendbu", "ozyaz.13");
 
-                    System.out.println("Database connected");
+            System.out.println("Database connected");
 
 
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }*/
-                
-                Connection con = null;
-        Class.forName("com.mysql.jdbc.Driver");
-        String username = "opendbu";
-        String password = "ozyaz.13";
-        String server = "139.179.139.112"; //remote server address
-        String db = "open";
-        String url = "jdbc:mysql://"+server+"/"+db;
-        con = DriverManager.getConnection(url, username, password);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(OnlineDBController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        /*   Connection con = null;
+         Class.forName("com.mysql.jdbc.Driver");
+         String username = "opendbu";
+         String password = "ozyaz.13";
+         String server = "139.179.139.112"; //remote server address
+         String db = "open";
+         String url = "jdbc:mysql://"+server+"/"+db;
+         con = DriverManager.getConnection(url, username, password);
+         } catch (ClassNotFoundException ex) {
+         Logger.getLogger(OnlineDBController.class.getName()).log(Level.SEVERE, null, ex);
+         }*/
     }
 }
