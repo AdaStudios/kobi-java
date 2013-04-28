220edc546c1853f2a5f2b4c21f0b64fc7a93980d
Osman <osmunalan@gmail.com>
2013-04-28 19:54:19 +0300

    temp order

--- src/com/ada/sme/view/TempAddStockView.java
+++ src/com/ada/sme/view/TempAddStockView.java
@@ -22,8 +22,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
