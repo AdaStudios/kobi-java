9ad24fa2d6380b290ad657c54d324ff7549d3c4b
Osman <osmunalan@gmail.com>
2013-04-28 19:53:41 +0300

    main

--- src/com/ada/sme/main/Main.java
+++ src/com/ada/sme/main/Main.java
@@ -13,11 +13,24 @@ package com.ada.sme.main;
 import com.ada.sme.view.CreateOwnerView;
 import com.ada.sme.view.LoginView;
 import com.ada.sme.controller.*;
+import com.ada.sme.view.TempAddStockView;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 //import com.ada.sme.view.LeftMenuView;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main
 {
@@ -26,12 +39,17 @@ public class Main
     public static File file;
     public static File file1;
     public static File file2;
+    public static File temp_sql;
     public static int image_id;
+    public static FileWriter sql_out;
+    public static FileReader sql_read;
+    public static DBController db;
     
     public static void main(String[] args)
     {
-        
-        file = new File("tmp_ssdfastock_list.txt");
+       
+        temp_sql = new File("tmp_sql.txt");
+        file = new File("tmp_stock_list.txt");
         file1 = new File("tmp_image_list.txt");
         file2 = new File("tmp_image_list_sql.txt");
         products = new ArrayList<Product>();
@@ -39,7 +57,7 @@ public class Main
         /*CreateDBController createDB =new CreateDBController();
         createDB.createDB();*/
         
-       DBController db = new DBController();
+       db = new DBController();
        ArrayList a = db.selectSingleRowDB("select * from employee");
        
         if (a.isEmpty())
@@ -59,7 +77,34 @@ public class Main
     }
 
 
-
+public static void write(String sql) throws IOException  {
+    
+            sql_out = new FileWriter(temp_sql,true);
+            BufferedWriter bw = new BufferedWriter(sql_out);
+            bw.write(sql+"\n");
+            bw.close();
+  }
+  
+  /** Read the contents of the given file. */
+  public static void read() throws IOException {
+    try{
+     sql_read = new FileReader(temp_sql);
+            BufferedReader sql_reader = new BufferedReader(sql_read);
+            String satir = "";
+             while((satir = sql_reader.readLine()) != null){                
+                 db.insDelUpDB(satir);             
+    
+             }
+             sql_reader.close();
+            BufferedWriter opennew = new BufferedWriter(sql_out);
+            System.out.print("Yazdım");
+            opennew.close();
+    } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    }    
+            
+  }
+  
     public static void setProducts(ArrayList<Product> p)
     {
         products.clear();
