8a652208abd3df3f506eb10515001eee09e0d553
Osman <osmunalan@gmail.com>
2013-04-28 16:34:48 +0300

    Orderview

--- src/com/ada/sme/view/OrderView.java
+++ src/com/ada/sme/view/OrderView.java
@@ -7,6 +7,7 @@ package com.ada.sme.view;
 import com.ada.sme.controller.DBController;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
@@ -29,6 +30,7 @@ public class OrderView extends javax.swing.JFrame {
 
         dbController = new DBController(); 
         updateOrderProduct();
+        updateOrderOption();
         
         ArrayList all = dbController.selectSingleRowDB("SELECT * FROM order_t, order_product WHERE order_product.order_id=order_t.order_id AND order_t.order_id="+id);
          
@@ -41,8 +43,23 @@ public class OrderView extends javax.swing.JFrame {
         cols[4]="Toplam";
          dtm.setColumnIdentifiers(cols);
          
-         MEV_list.setModel(dtm);        
-        
+         MEV_list1.setModel(dtm);    
+         
+         updateOrderOption();
+         
+         dtm=dbController.selectDB("SELECT model, value FROM order_product, order_option WHERE order_product.order_product_id=order_option.order_product_id AND order_product.order_id="+all.get(0).toString());
+         MEV_list.setModel(dtm);
+         
+         double total = 0;
+         for (int i = 0; i < MEV_list1.getRowCount(); i++) {
+             total+= Double.valueOf(MEV_list1.getValueAt(i,4).toString());
+         }
+           jLabel28.setText(total+"");
+           total+=7;
+           jLabel34.setText(total+"");
+           
+        // MEV_list.getValueAt(WIDTH, WIDTH)
+         
          jLabel12.setText(all.get(0).toString());
          jLabel11.setText(all.get(31).toString());
          jLabel17.setText(all.get(7).toString()+" "+all.get(8).toString() );
@@ -59,10 +76,10 @@ public class OrderView extends javax.swing.JFrame {
          jLabel25.setText(all.get(20).toString()+" "+all.get(21).toString());
          jLabel26.setText(all.get(23).toString()+" "+all.get(22).toString()+" "+all.get(25).toString());
          
-         jLabel28.setText(all.get(36).toString());
-         jLabel30.setText(all.get(37).toString());
-         jLabel32.setText(all.get(40).toString());
-         jLabel34.setText(all.get(39).toString());
+       
+         jLabel32.setText(all.get(37).toString());
+        
+       /*  jLabel34.setText(all.get(39).toString());*/
          
     }
     
@@ -88,13 +105,14 @@ public class OrderView extends javax.swing.JFrame {
         dtm=dbController.selectDB("SELECT * FROM order_option");
 
         if(dtm.getRowCount()==0){
-         dbController.updateOrderProductDBFromOnline("SELECT order_option_id, order_id, order_product_id,name,value FROM oc_order_option");
+         dbController.updateOrderOptionDBFromOnline("SELECT order_option_id, order_id, order_product_id,name,value FROM oc_order_option");
         }else{
-             int id2 = DBController.getLastID("SELECT order_option_id from order_option order by order_option DESC");
+             int id2 = DBController.getLastID("SELECT order_option_id from order_option order by order_option_id DESC");
              System.out.println(id2+"Option Last Id\n\n");
             
-           dbController.updateOrderProductDBFromOnline("Select order_option_id, order_id, order_product_id, name, value FROM oc_order_option WHERE order_option_id>"+id2);          
+           dbController.updateOrderOptionDBFromOnline("Select order_option_id, order_id, order_product_id, name, value FROM oc_order_option WHERE order_option_id>"+id2);          
         }
+        
     }
 
     /**
@@ -136,94 +154,97 @@ public class OrderView extends javax.swing.JFrame {
         jLabel28 = new javax.swing.JLabel();
         jLabel30 = new javax.swing.JLabel();
         jLabel29 = new javax.swing.JLabel();
-        jLabel31 = new javax.swing.JLabel();
-        jLabel32 = new javax.swing.JLabel();
         jLabel34 = new javax.swing.JLabel();
         jLabel33 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        MEV_list1 = new javax.swing.JTable();
+        jLabel31 = new javax.swing.JLabel();
+        jLabel32 = new javax.swing.JLabel();
 
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setMinimumSize(new java.awt.Dimension(1000, 600));
-        setPreferredSize(new java.awt.Dimension(1000, 600));
+        setMinimumSize(new java.awt.Dimension(900, 600));
+        setPreferredSize(new java.awt.Dimension(900, 600));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setText("Meşrutiyet Cad. Kızılay No:32 Çankaya/Ankara");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, -1, -1));
 
         jLabel3.setText("(0312) 290 40 64");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, -1, -1));
 
         jLabel4.setText("www.magazam.com");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, -1, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
 
         jLabel5.setText("magazam@magazam.com");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
 
         jLabel7.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel7.setText("Ekleme Tarihi:");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 50, -1, -1));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 30, -1, -1));
 
         jLabel8.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel8.setText("Sipariş No:");
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 80, -1, -1));
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 60, -1, -1));
 
         jLabel9.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel9.setText("KARGO ADRESİ");
-        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 180, -1, -1));
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 160, -1, -1));
 
         jLabel10.setText("fax");
-        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 330, -1, -1));
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, -1, -1));
 
         jLabel11.setText("2013");
-        getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 50, -1, -1));
+        getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 30, -1, -1));
 
         jLabel12.setText("0");
         jLabel12.setMinimumSize(new java.awt.Dimension(500, 500));
-        getContentPane().add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 80, -1, -1));
+        getContentPane().add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 60, -1, -1));
 
         jLabel13.setText("Kapıda");
-        getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 120, -1, -1));
+        getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 100, -1, -1));
 
         jLabel14.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel14.setText("Ödeme Methodu");
-        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 120, -1, -1));
+        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 100, -1, -1));
 
         jLabel15.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel15.setText("FATURA ADRESİ");
-        getContentPane().add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, -1, -1));
+        getContentPane().add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, -1, -1));
 
         jLabel16.setText("Örnek Mağaza");
-        getContentPane().add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, -1, -1));
+        getContentPane().add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, -1, -1));
 
         jLabel17.setText("Ad Soyad");
-        getContentPane().add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, -1, -1));
+        getContentPane().add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 190, -1, -1));
 
         jLabel18.setText("İşyeri");
-        getContentPane().add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, -1, -1));
+        getContentPane().add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, -1, -1));
 
         jLabel19.setText("adress1");
-        getContentPane().add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, -1, -1));
+        getContentPane().add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, -1, -1));
 
         jLabel20.setText("city/zone/postcode");
-        getContentPane().add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, -1, -1));
+        getContentPane().add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, -1, -1));
 
         jLabel21.setText("mail");
-        getContentPane().add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, -1, -1));
+        getContentPane().add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, -1, -1));
 
         jLabel22.setText("tel");
-        getContentPane().add(jLabel22, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, -1, -1));
+        getContentPane().add(jLabel22, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, -1, -1));
 
         jLabel23.setText("Ad Soyad");
-        getContentPane().add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 210, -1, -1));
+        getContentPane().add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 190, -1, -1));
 
         jLabel24.setText("İşyeri");
-        getContentPane().add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 240, -1, -1));
+        getContentPane().add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 220, -1, -1));
 
         jLabel25.setText("adress1");
-        getContentPane().add(jLabel25, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 270, -1, -1));
+        getContentPane().add(jLabel25, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 250, -1, -1));
 
         jLabel26.setText("city/zone/postcode");
-        getContentPane().add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 310, -1, -1));
+        getContentPane().add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 290, -1, -1));
 
+        MEV_list.setEnabled(false);
         MEV_list.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 MEV_listMouseClicked(evt);
@@ -231,47 +252,62 @@ public class OrderView extends javax.swing.JFrame {
         });
         jScrollPane2.setViewportView(MEV_list);
 
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 360, 630, 130));
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 340, 230, 120));
 
         jLabel27.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel27.setText("Ara Toplam:");
-        getContentPane().add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 370, -1, -1));
+        getContentPane().add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 470, -1, -1));
 
         jLabel28.setText("0");
-        getContentPane().add(jLabel28, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 370, -1, -1));
+        getContentPane().add(jLabel28, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 470, -1, -1));
 
         jLabel30.setText("7");
-        getContentPane().add(jLabel30, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 400, -1, -1));
+        getContentPane().add(jLabel30, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 500, -1, -1));
 
         jLabel29.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel29.setText("Kargo:");
-        getContentPane().add(jLabel29, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 400, -1, -1));
-
-        jLabel31.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
-        jLabel31.setText("KDV:");
-        getContentPane().add(jLabel31, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 430, -1, -1));
-
-        jLabel32.setText("0");
-        getContentPane().add(jLabel32, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 430, -1, -1));
+        getContentPane().add(jLabel29, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 500, -1, -1));
 
         jLabel34.setText("0");
-        getContentPane().add(jLabel34, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 460, -1, -1));
+        getContentPane().add(jLabel34, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 530, -1, -1));
 
         jLabel33.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         jLabel33.setText("Toplam");
-        getContentPane().add(jLabel33, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 460, -1, -1));
+        getContentPane().add(jLabel33, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 530, -1, -1));
+
+        MEV_list1.setEnabled(false);
+        MEV_list1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                MEV_list1MouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(MEV_list1);
+
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 340, 600, 120));
+
+        jLabel31.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
+        jLabel31.setText("Kargo Durumu:");
+        getContentPane().add(jLabel31, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 470, -1, -1));
+
+        jLabel32.setText("0");
+        getContentPane().add(jLabel32, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 470, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void MEV_listMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MEV_listMouseClicked
-        String id = MEV_list.getValueAt(MEV_list.getSelectedRow(), 0).toString();
+       /* String id = MEV_list.getValueAt(MEV_list.getSelectedRow(), 0).toString();
         UpdateEmployeeView updateemployeeView=new UpdateEmployeeView(id);
         MainFrame.main_anapanel.removeAll();
         MainFrame.main_anapanel.add(updateemployeeView);
-        MainFrame.main_anapanel.validate();
+        MainFrame.main_anapanel.validate();*/
     }//GEN-LAST:event_MEV_listMouseClicked
+
+    private void MEV_list1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MEV_list1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MEV_list1MouseClicked
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable MEV_list;
+    private javax.swing.JTable MEV_list1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
@@ -305,5 +341,6 @@ public class OrderView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     // End of variables declaration//GEN-END:variables
 }
